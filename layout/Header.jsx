e29b18d4c9f5bfaa056e25d098fa3059db505a38
bipindubey-technoark <bipin.dubey@technoark.io>
2020-08-08 17:47:07 +0530

    changes

--- layout/Header.jsx
+++ layout/Header.jsx
@@ -36,57 +36,12 @@ function Header(props) {
   const [businesses, setBusinesses] = useState();
   const [header, setHeader] = useState("");
 
-  async function fetchAllBusiness() {
-    const { data } = await api.get('business');
-    setBusinesses(data);
-  }
-  const [{businessId}, dispatch] = useAuth();
-
-  useEffect(() => {
-    fetchAllBusiness();
-    setHeader(businessId)
-  }, []);
-  const handleChange = async event => {
-    try {
-      let {
-        data: { authorization },
-      } = await api.get(`business/token/${event.target.value}`);
-      dispatch({ type: 'UPDATE_BUSINESS_TOKEN', payload: authorization });
-      localStorage.setItem('authToken', authorization);
-      dispatch({ type: 'UPDATE_BUSINESS_ID', payload: event.target.value });
-      setHeader(event.target.value);
-      router.push(`/dashboard`);
-    } catch (error) {
-      console.log(error);
-    }
-    // router.push(`/businesses/edit/[id]`, `/businesses/edit/${event.target.value}`);
-  };
-
   return (
     <AppBar elevation={0} className={classes.appBar} position="fixed">
       <Toolbar>
         <Typography className={classes.pageTitle} variant="h5" noWrap>
           {pageName}
         </Typography>
-
-        <Select
-          margin="dense"
-          variant="filled"
-          value={header}
-          onChange={handleChange}
-          style={{ color: 'white' }}
-          classes={{ root: classes.selectRoot }}
-        >
-          <MenuItem value="Select" selected>
-            <em>Select Business</em>
-          </MenuItem>
-          {businesses &&
-            businesses.map(b => (
-              <MenuItem key={b.id} value={b.id}>
-                {b.businessName}
-              </MenuItem>
-            ))}
-        </Select>
         <CustomMenu/>
         {/* <CustomMenu accountData={accountData} securityData={securityData} /> */}
       </Toolbar>
